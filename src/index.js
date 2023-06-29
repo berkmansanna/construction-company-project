1ba8e1247554e7e55ced616e677f16cc75442055
berkmansanna <90900129+berkmansanna@users.noreply.github.com>
2023-06-29 10:18:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <BrowserRouter basename="/berkmansanna/">
+  <BrowserRouter basename="/construction-company-project/">
   <App />
 </BrowserRouter>
   </React.StrictMode>
